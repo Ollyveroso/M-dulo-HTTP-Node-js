e7a7fa3edf165abe6b48904945aadfac09ba3cfb
Ollyveroso <126161749+Ollyveroso@users.noreply.github.com>
2023-09-21 10:43:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var dt = require('./myfirstmodule.js');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("uau:" + dt.myDateTime());
+  res.write("codiguinho do Ollyver:" + dt.myDateTime());
   res.end();
 }).listen(5011);
 
